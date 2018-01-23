430fa3b023e5062baa5c5e9130447d962df87951
Sythelux Rikd <dersyth@gmail.com>
2018-01-23 21:14:51 +0000

    * finished pagination of online

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,10 +1,9 @@
-import six
-from PicartoClientAPI import PublicApi, rest, logger, OnlineChannels
-
-from six import iteritems
+from PicartoClientAPI import PublicApi, rest, logger, OnlineDetails, ApiClient
+from pagination import PaginationStore
 
 public_api = PublicApi()
 api_client = ApiClient()
+pages = PaginationStore()
 
 rest.HTTP = HTTP
 logger.Log = Log
@@ -53,19 +52,19 @@ def MainMenu():
     )
 
     oc.add(DirectoryObject(
-        key=Callback(OnlineSubMenu, key='/c', title=L('Online')),
+        key=Callback(OnlineSubMenu, title=L('Online')),
         title=u'%s' % L('Online'),
     ))
     Log.Debug("SubMenu: online")
 
     oc.add(DirectoryObject(
-        key=Callback(CategoriesSubMenu, key='/categories', title=L('Categories')),
+        key=Callback(CategoriesSubMenu, title=L('Categories')),
         title=u'%s' % L('Categories'),
     ))
     Log.Debug("SubMenu: categories")
 
     oc.add(DirectoryObject(
-        key=Callback(EventsSubMenu, key='/events', title=L('Events')),
+        key=Callback(EventsSubMenutitle=L('Events')),
         title=u'%s' % L('Events'),
     ))
     Log.Debug("SubMenu: events")
@@ -79,47 +78,61 @@ def MainMenu():
     return oc
 
 
-@route(PREFIX + '/c')
-def OnlineSubMenu(key, title, **kwargs):
+def buildSummary(details):
+    """ :type details OnlineDetails"""
+    Log.Debug("buildSummary " + "")
+    return "\n".join('%s' % x for x in details.languages)
+
+
+@route(PREFIX + '/online')
+def OnlineSubMenu(title, page=1, **kwargs):
     try:
         Log.Debug("OnlineSubMenu")
-        Log.Debug("Online: " + str(key) + ", " + str(title))
-        online_channels_list = public_api.online_get(adult=Prefs['filter_adult'],
-                                                     gaming=Prefs['filter_gaming'])  # type: list
-        Log.Debug(online_channels_list)
-        for channel_dict in online_channels_list:
-            Log.Debug(channel_dict)
-            details = api_client.i__deserialize_model(channel_dict, OnlineDetails)  # type: OnlineDetails
-            Log.Debug(details)
-            return DirectoryObject(key=Callback(showArtist, id=details.user_id),
-                                   title=details.name,
-                                   tagline=details.category,
-                                   art=details.thumbnails.web
-                                   )
+        Log.Debug("Online: " + str(title))
+        page = int(page)
+        if not pages.online_pages:
+            buffer_list = list()
+            online_channels_list = public_api.online_get(adult=Prefs['filter_adult'],
+                                                         gaming=Prefs['filter_gaming'])  # type: list
+
+            for channel_dict in online_channels_list:
+                # Log.Debug(channel_dict)
+                details = api_client.deserialize_model(channel_dict, OnlineDetails)  # type: OnlineDetails
+                # Log.Debug(details)
+
+                buffer_list.append(details)
+            pages.online_pages = list(PaginationStore.grouper(int(Prefs['elements_per_side']), buffer_list))
+
+        online_page_length = len(pages.online_pages)
+        page = abs(page) % online_page_length
+        oc = ObjectContainer(
+            title2=u'%s' % title + " (" + str(page) + "/" + str(online_page_length) + ")",
+            art=None,
+            content=ContainerContent.Albums
+        )
+        # Log.Debug(str(page) + ", " + str(pages.online_pages[page]))
+        for details in pages.online_pages[page]:
+            oc.add(VideoClipObject(url=StreamURLForName(details.name),
+                                   title=details.title,
+                                   thumb=details.thumbnails.mobile,
+                                   # summary=buildSummary(details),
+                                   # rating=, <-stars rating
+                                   content_rating=L("adult") if details.adult else L("everyone"),
+                                   # duration=float('inf'),# needs int
+                                   # year=current,
+                                   # genres=details.category,
+                                   # writers=writers_a,
+                                   # directors=directors_a,
+                                   # roles=roles_a,
+                                   studio=details.name
+                                   ))
+        if page < online_page_length:
+            page += 1
+            oc.add(NextPageObject(key=Callback(OnlineSubMenu, title=L("back"), page=page)))
+        return oc
     except Exception as e:
         Log.Exception("OnlineSubMenu had an exception")
-        return ContentNotFound()
-
-    # oc = ObjectContainer(
-    #     title2=u'%s' % title,
-    #     art=None,
-    #     content=ContainerContent.Shows
-    # )
-    # Log.Debug("ObjectContainer")
-    #
-    # for item in items:
-    #     Log.Debug(item)
-    #     oc.add(URLService.MetadataObjectForURL(STREAM_BASE % item['name']))
-    # oc.add(DirectoryObject(key=Callback(showArtist, id=item['name']),
-    #                        title=item['name'],
-    #                        tagline=item['category'],
-    #                        art=THUMB_BASE % item['name'].lower()
-    #                        )
-    #        )
-
-    Log.Debug("return")
     return ContentNotFound()
-    # return oc
 
 
 @route(PREFIX + '/categories')
@@ -135,32 +148,32 @@ def EventsSubMenu(key, title, **kwargs):
 
 
 @route(PREFIX + '/artist')
-def showArtist(id, **kwargs):
-    #     # oc = ObjectContainer(title2=u'%s' % id)
-    #     # oc.add(URLService.MetadataObjectForURL("https://1-edge5-us-east.picarto.tv/mp4/Swizzlestix.mp4?token=public&con=1503173554275&ticket=0&type=0&scope=0"))
-    oc = ObjectContainer(title2=u'%s' % id)
-    oc.add()
+def showArtist(name, **kwargs):
+    Log.Debug("showArtist " + name + ", ".join('%s=%r' % x for x in kwargs.iteritems()))
+    oc = ObjectContainer(title2=u'%s' % name)
+    try:
+        oc.add(URLService.MediaObjectsForURL(
+            "https://1-edge1-eu-west.picarto.tv/mp4/LiLaiRa.mp4?token=public&ticket=0&con=1516735777815&type=0&scope=0"))
+        # oc.add(URLService.MediaObjectsForURL(StreamURLForName(name)))
+    except Exception as e:
+        Log.Exception("showArtist had an exception")
+        return ContentNotFound()
     return oc
 
 
-class Api(object):
-    @classmethod
-    def Request(cls, uri, params=None):
-        Log.Debug("Request")
-        uri = P_API + uri
-
-        if not params is None:
-            uri = uri + '?' + params
-
-        try:
-            Log.Debug(uri)
-            ret = JSON.ObjectFromURL(uri)
-            if 'error' in ret:
-                Log.Error(ret['error']['message'])
-                return None
-            return ret
-        except:
-            return None
+def StreamURLForName(name):
+    Log.Debug("StreamURLForName " + STREAM_BASE % name)
+    return STREAM_BASE % name
+    # the following is not working without javascript
+    # Log.Debug("StreamURLForName "+name)
+    # search_page = HTML.ElementFromURL("https://picarto.tv/" + name)
+    # Log.Debug(search_page)
+    # items = search_page.xpath("//*[@id='picarto-player-1_html5_api']")
+    # Log.Debug(items)
+    # for item in items:
+    #     Log.Debug(item)
+    #     return item.get("src")
+    # return ""
 
 
 def ContentNotFound():
